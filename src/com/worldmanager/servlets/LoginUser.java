473d758f5b0c31474f04e1fbf1785e7b9788c7a4
zkocen <zkocen@gmail.com>
2014-10-29 21:10:32 +0000

    added cookies in login user

--- src/com/worldmanager/servlets/LoginUser.java
+++ src/com/worldmanager/servlets/LoginUser.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,20 +27,37 @@ public class LoginUser extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String uid = request.getParameter("uid");
 		String pwd = request.getParameter("pwd");
+		int authLevel = 1;
 		
 		HttpSession s = request.getSession();
 		s.setAttribute("uid", uid);
 		s.setAttribute("pwd", pwd);
+		s.setAttribute("authlevel", authLevel);
 		
-		String destination = "ShowUserCredentials.jsp";
-		if (request.getParameter("noSession") != null) {
-			String noSession = request.getParameter("noSession");
-			if (noSession.equalsIgnoreCase("ON")) {
-				destination = "showUserCredentials2.jsp";
+		String destination = "listCities.jsp";
+		if (request.getAttribute("dest") != null) {
+			destination = (String)request.getAttribute("dest");
+		}
+		if (destination != null && destination.equals("listcities")) {
+			destination = "listCities.jsp";
+		}
+		if (request.getParameter("rememberMe") != null) {
+			String rememberMe = request.getParameter("rememberMe");
+			if (rememberMe.equalsIgnoreCase("ON")) {
+				int cookieLife = 3600*24*7;
+				Cookie uidCook = new Cookie("credentials_uid", uid);
+				uidCook.setMaxAge(cookieLife);
+				response.addCookie(uidCook);
+				Cookie pwdCook = new Cookie("credentials_pwd", pwd);
+				pwdCook.setMaxAge(cookieLife);
+				response.addCookie(pwdCook);
 			}
 		}
-		
-		response.sendRedirect(response.encodeRedirectURL(destination));
+		if (authLevel < 1 || uid == null || uid == "") {
+			response.sendRedirect(response.encodeRedirectURL("login.jsp"));
+		} else {
+			response.sendRedirect(response.encodeRedirectURL(destination));
+		}
 	}
 
 }
